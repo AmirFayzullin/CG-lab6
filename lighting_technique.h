2af1e1e5c46f2b50b6275bf7581f81f3f0c56f98
Amir <amir.fayzullin@gmail.com>
2022-05-25 18:26:00 +0500

    tutorial 42

--- lighting_technique.h
+++ lighting_technique.h
@@ -1,5 +1,22 @@
-#ifndef SKINNING_TECHNIQUE_H
-#define	SKINNING_TECHNIQUE_H
+/*
+        Copyright 2011 Etay Meiri
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef LIGHTING_TECHNIQUE_H
+#define	LIGHTING_TECHNIQUE_H
 
 #include "technique.h"
 #include "ogldev_math_3d.h"
@@ -60,40 +77,42 @@ struct SpotLight : public PointLight
     }
 };
 
-class SkinningTechnique : public Technique {
+class LightingTechnique : public Technique {
 public:
 
-    static const uint MAX_POINT_LIGHTS = 2;
-    static const uint MAX_SPOT_LIGHTS = 2;
-    static const uint MAX_BONES = 100;
+    static const unsigned int MAX_POINT_LIGHTS = 2;
+    static const unsigned int MAX_SPOT_LIGHTS = 2;
 
-    SkinningTechnique();
+    LightingTechnique();
 
     virtual bool Init();
 
     void SetWVP(const Matrix4f& WVP);
-    void SetPrevWVP(const Matrix4f& PrevWVP);
+    void SetLightWVP(const Matrix4f& LightWVP);
     void SetWorldMatrix(const Matrix4f& WVP);
-    void SetColorTextureUnit(uint TextureUnit);
+    void SetColorTextureUnit(unsigned int TextureUnit);
+    void SetShadowMapTextureUnit(unsigned int TextureUnit);
     void SetDirectionalLight(const DirectionalLight& Light);
-    void SetPointLights(uint NumLights, const PointLight* pLights);
-    void SetSpotLights(uint NumLights, const SpotLight* pLights);
+    void SetPointLights(unsigned int NumLights, const PointLight* pLights);
+    void SetSpotLights(unsigned int NumLights, const SpotLight* pLights);
     void SetEyeWorldPos(const Vector3f& EyeWorldPos);
     void SetMatSpecularIntensity(float Intensity);
     void SetMatSpecularPower(float Power);
-    void SetBoneTransform(uint Index, const Matrix4f& Transform);
-    void SetPrevBoneTransform(uint Index, const Matrix4f& Transform);
-    
+    void SetShadowMapSize(float Width, float Height);
+
 private:
     
     GLuint m_WVPLocation;
+    GLuint m_LightWVPLocation;
     GLuint m_WorldMatrixLocation;
-    GLuint m_colorTextureLocation;
+    GLuint m_samplerLocation;
+    GLuint m_shadowMapLocation;
     GLuint m_eyeWorldPosLocation;
     GLuint m_matSpecularIntensityLocation;
     GLuint m_matSpecularPowerLocation;
     GLuint m_numPointLightsLocation;
     GLuint m_numSpotLightsLocation;
+    GLuint m_shadowMapSizeLocation;
 
     struct {
         GLuint Color;
@@ -127,10 +146,7 @@ private:
             GLuint Exp;
         } Atten;
     } m_spotLightsLocation[MAX_SPOT_LIGHTS];
-    
-    GLuint m_boneLocation[MAX_BONES];
-    GLuint m_prevBoneLocation[MAX_BONES];
 };
 
 
-#endif	/* SKINNING_TECHNIQUE_H */
+#endif	/* LIGHTING_TECHNIQUE_H */
--- main.cpp
+++ main.cpp
@@ -2,27 +2,25 @@
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 #include <string>
-#ifndef WIN32
-#include <sys/time.h>
-#include <unistd.h>
-#endif
-#include <sys/types.h>
 
 #include "ogldev_engine_common.h"
 #include "ogldev_app.h"
+#include "ogldev_backend.h"
 #include "ogldev_util.h"
 #include "ogldev_pipeline.h"
 #include "ogldev_camera.h"
-#include "skinning_technique.h"
-#include "motion_blur_technique.h"
+#include "ogldev_texture.h"
+#include "lighting_technique.h"
 #include "ogldev_glut_backend.h"
-#include "ogldev_skinned_mesh.h"
-#include "intermediate_buffer.h"
+#include "ogldev_basic_mesh.h"
+#include "shadow_map_technique.h"
+#include "shadow_map_fbo.h"
 
 using namespace std;
 
-#define WINDOW_WIDTH  1080 
+#define WINDOW_WIDTH  1080
 #define WINDOW_HEIGHT 768
+
 
 class Main : public ICallbacks, public OgldevApp
 {
@@ -41,88 +39,87 @@ public:
     const std::string SPIDER = CONTENT_PATH + "spider.obj";
     const std::string SPHERE_OBJ = CONTENT_PATH + "sphere.obj";
     const std::string MODEL = CONTENT_PATH + "boblampclean.md5mesh";
-
     Main() 
     {
         m_pGameCamera = NULL;
-        m_pSkinningTech = NULL;
-        m_pMotionBlurTech = NULL;
-        m_directionalLight.Color = Vector3f(1.0f, 1.0f, 1.0f);
-        m_directionalLight.AmbientIntensity = 0.55f;
-        m_directionalLight.DiffuseIntensity = 1.0f;
-        m_directionalLight.Direction = Vector3f(1.0f, 0.0, 0.0);
+        m_pLightingEffect = NULL;
+        m_pShadowMapEffect = NULL;
+        m_scale = 0.0f;
+        m_spotLight.AmbientIntensity = 0.5f;
+        m_spotLight.DiffuseIntensity = 1.0f;
+        m_spotLight.Color = Vector3f(1.0f, 1.0f, 1.0f);
+        m_spotLight.Attenuation.Linear = 0.01f;
+        m_spotLight.Position  = Vector3f(-20.0, 20.0, 1.0f);
+        m_spotLight.Direction = Vector3f(1.0f, -1.0f, 0.0f);
+        m_spotLight.Cutoff =  20.0f;
 
         m_persProjInfo.FOV = 60.0f;
         m_persProjInfo.Height = WINDOW_HEIGHT;
         m_persProjInfo.Width = WINDOW_WIDTH;
         m_persProjInfo.zNear = 1.0f;
         m_persProjInfo.zFar = 100.0f;  
-        
-        m_position = Vector3f(0.0f, 0.0f, 6.0f);      
     }
 
     ~Main()
     {
-        SAFE_DELETE(m_pSkinningTech);
-        SAFE_DELETE(m_pMotionBlurTech);
+        SAFE_DELETE(m_pLightingEffect);
+        SAFE_DELETE(m_pShadowMapEffect);
         SAFE_DELETE(m_pGameCamera);
     }    
 
     bool Init()
     {
-        Vector3f Pos(0.0f, 3.0f, -1.0f);
-        Vector3f Target(0.0f, 0.0f, 1.0f);
+        Vector3f Pos(3.0f, 8.0f, -10.0f);
+        Vector3f Target(0.0f, -0.5f, 1.0f);
         Vector3f Up(0.0, 1.0f, 0.0f);
 
+        if (!m_shadowMapFBO.Init(WINDOW_WIDTH, WINDOW_HEIGHT)) {
+            return false;
+        }
+
         m_pGameCamera = new Camera(WINDOW_WIDTH, WINDOW_HEIGHT, Pos, Target, Up);
-        
-        if (!m_intermediateBuffer.Init(WINDOW_WIDTH, WINDOW_HEIGHT)) {
-            printf("Error initializing the intermediate buffer\n");
-            return false;
-        }
       
-        m_pSkinningTech = new SkinningTechnique();
+        m_pLightingEffect = new LightingTechnique();
 
-        if (!m_pSkinningTech->Init()) {
-            printf("Error initializing the skinning technique\n");
+        if (!m_pLightingEffect->Init()) {
+            printf("Error initializing the lighting technique\n");
             return false;
         }
 
-        m_pSkinningTech->Enable();
+        m_pLightingEffect->Enable();
 
-        m_pSkinningTech->SetColorTextureUnit(COLOR_TEXTURE_UNIT_INDEX);
-        m_pSkinningTech->SetDirectionalLight(m_directionalLight);
-        m_pSkinningTech->SetMatSpecularIntensity(0.0f);
-        m_pSkinningTech->SetMatSpecularPower(0);
+        m_pLightingEffect->SetColorTextureUnit(COLOR_TEXTURE_UNIT_INDEX);
+        m_pLightingEffect->SetShadowMapTextureUnit(SHADOW_TEXTURE_UNIT_INDEX);		
+        m_pLightingEffect->SetSpotLights(1, &m_spotLight);
+        m_pLightingEffect->SetShadowMapSize((float)WINDOW_WIDTH, (float)WINDOW_HEIGHT);
 
-        m_pMotionBlurTech = new MotionBlurTechnique();
+        m_pShadowMapEffect = new ShadowMapTechnique();
 
-        if (!m_pMotionBlurTech->Init()) {
-            printf("Error initializing the motion blur technique\n");
+        if (!m_pShadowMapEffect->Init()) {
+            printf("Error initializing the shadow map technique\n");
+            return false;
+        }        
+
+		if (!m_quad.LoadMesh(QUAD_OBJ)) {
             return false;
         }
-        
-        m_pMotionBlurTech->Enable();
-        m_pMotionBlurTech->SetColorTextureUnit(COLOR_TEXTURE_UNIT_INDEX);
-        m_pMotionBlurTech->SetMotionTextureUnit(MOTION_TEXTURE_UNIT_INDEX);
 
-        if (!m_mesh.LoadMesh(MODEL)) {
-            printf("Mesh load failed\n");
-            return false;            
-        }
-        
-        m_mesh.BoneTransform(0.0f, m_prevTransforms);
+		m_pGroundTex = new Texture(GL_TEXTURE_2D, TEXTURE);
 
-        if (!m_quad.LoadMesh(QUAD_R_OBJ)) {
-            printf("Quad mesh load failed\n");
-            return false;            
+        if (!m_pGroundTex->Load()) {
+            return false;
         }
-        
+
+		if (!m_mesh.LoadMesh(PHOENIX)) {
+			return false;
+		}
+
 #ifdef FREETYPE
         if (!m_fontRenderer.InitFontRenderer()) {
             return false;
         }
-#endif        	      
+#endif
+        
         return true;
     }
 
@@ -131,107 +128,115 @@ public:
         GLUTBackendRun(this);
     }
     
-    /*Главная функция рендера*/
+
     virtual void RenderSceneCB()
     {   
         CalcFPS();
-              
+        
+        m_scale += 0.05f;
+
         m_pGameCamera->OnRender();
 
+        ShadowMapPass();
         RenderPass();
         
-        MotionBlurPass();
-                              
-        RenderFPS();       
-        
+        RenderFPS();
+
         glutSwapBuffers();
     }
-    float tmp = 0.00f;
-    /*Проход рендера*/
+
+    void ShadowMapPass()
+    {
+        glCullFace(GL_FRONT);
+        
+        m_shadowMapFBO.BindForWriting();
+
+        glClear(GL_DEPTH_BUFFER_BIT);
+
+        m_pShadowMapEffect->Enable();
+
+        Pipeline p;
+        p.Scale(0.1f, 0.1f, 0.1f);
+        p.Rotate(0.0f, m_scale, 0.0f);
+        p.WorldPos(0.0f, 0.0f, 3.0f);
+        p.SetCamera(m_spotLight.Position, m_spotLight.Direction, Vector3f(0.0f, 1.0f, 0.0f));
+        p.SetPerspectiveProj(m_persProjInfo);
+        m_pShadowMapEffect->SetWVP(p.GetWVPTrans());
+        m_mesh.Render();
+        
+        glBindFramebuffer(GL_FRAMEBUFFER, 0);
+    }
+
     void RenderPass()
     {
-        m_intermediateBuffer.BindForWriting();
-
+        glCullFace(GL_BACK);
+        
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-        m_pSkinningTech->Enable();
-        
-        vector<Matrix4f> Transforms;
-               
-        float RunningTime = GetRunningTime();
-
-        m_mesh.BoneTransform(RunningTime, Transforms);
-        
-        for (uint i = 0 ; i < Transforms.size() ; i++) {
-            m_pSkinningTech->SetBoneTransform(i, Transforms[i]);
-            m_pSkinningTech->SetPrevBoneTransform(i, m_prevTransforms[i]);
-        }                
-               
-        m_pSkinningTech->SetEyeWorldPos(m_pGameCamera->GetPos());
-        
-        m_pipeline.SetCamera(m_pGameCamera->GetPos(), m_pGameCamera->GetTarget(), m_pGameCamera->GetUp());
-        m_pipeline.SetPerspectiveProj(m_persProjInfo);           
-        m_pipeline.Scale(0.1f, 0.1f, 0.1f);   
-        tmp += 1.01f;
+        m_pLightingEffect->Enable();
              
-        Vector3f Pos(m_position);
-        m_pipeline.WorldPos(Pos);        
-        m_pipeline.Rotate(270.0f, 180.0f + tmp, 0.0f);       
-        m_pSkinningTech->SetWVP(m_pipeline.GetWVPTrans());
-        m_pSkinningTech->SetWorldMatrix(m_pipeline.GetWorldTrans());            
+        m_shadowMapFBO.BindForReading(GL_TEXTURE1);
+
+        Pipeline p;
+        p.SetPerspectiveProj(m_persProjInfo);           
+        p.Scale(10.0f, 10.0f, 10.0f);
+        p.WorldPos(0.0f, 0.0f, 1.0f);
+        p.Rotate(90.0f, 0.0f, 0.0f);
+        p.SetCamera(m_pGameCamera->GetPos(), m_pGameCamera->GetTarget(), m_pGameCamera->GetUp());
+
+        m_pLightingEffect->SetWVP(p.GetWVPTrans());
+        m_pLightingEffect->SetWorldMatrix(p.GetWorldTrans());
+        p.SetCamera(m_spotLight.Position, m_spotLight.Direction, Vector3f(0.0f, 1.0f, 0.0f));
        
-        m_mesh.Render();        
-        
-        m_prevTransforms = Transforms;
-    }
-    
-    /*Привязываем промежуточный буфер для чтения и
-      рендерим прямоугольник на весь экран*/
-
-    void MotionBlurPass()
-    {
-        m_intermediateBuffer.BindForReading();
-
-        glClear(GL_COLOR_BUFFER_BIT|GL_DEPTH_BUFFER_BIT);
-
-        m_pMotionBlurTech->Enable();
-       
+        m_pLightingEffect->SetLightWVP(p.GetWVPTrans());
+        m_pLightingEffect->SetEyeWorldPos(m_pGameCamera->GetPos());
+        m_pGroundTex->Bind(GL_TEXTURE0);
         m_quad.Render();
+ 
+        p.Scale(0.1f, 0.1f, 0.1f);
+        p.Rotate(0.0f, m_scale, 0.0f);
+        p.WorldPos(0.0f, 0.0f, 3.0f);
+        p.SetCamera(m_pGameCamera->GetPos(), m_pGameCamera->GetTarget(), m_pGameCamera->GetUp());
+        m_pLightingEffect->SetWVP(p.GetWVPTrans());
+        m_pLightingEffect->SetWorldMatrix(p.GetWorldTrans());
+        p.SetCamera(m_spotLight.Position, m_spotLight.Direction, Vector3f(0.0f, 1.0f, 0.0f));
+        m_pLightingEffect->SetLightWVP(p.GetWVPTrans());
+
+        m_mesh.Render();        
     }
 
 
-    virtual void KeyboardCB(OGLDEV_KEY OgldevKey)
-    {
-        switch (OgldevKey) {
-        case OGLDEV_KEY_ESCAPE:
-        case OGLDEV_KEY_q:
-                GLUTBackendLeaveMainLoop();
-                break;
-        default:
-                m_pGameCamera->OnKeyboard(OgldevKey);
-        }
-    }
+	void KeyboardCB(OGLDEV_KEY OgldevKey)
+	{
+		switch (OgldevKey) {
+		case OGLDEV_KEY_ESCAPE:
+		case OGLDEV_KEY_q:
+			OgldevBackendLeaveMainLoop();
+			break;
+		default:
+			m_pGameCamera->OnKeyboard(OgldevKey);
+		}
+	}
 
 
-    virtual void PassiveMouseCB(int x, int y)
-    {
-        m_pGameCamera->OnMouse(x, y);
-    }
-    
+	virtual void PassiveMouseCB(int x, int y)
+	{
+		m_pGameCamera->OnMouse(x, y);
+	}
+
     
 private:
-        
-    SkinningTechnique* m_pSkinningTech;
-    MotionBlurTechnique* m_pMotionBlurTech;
+    
+    LightingTechnique* m_pLightingEffect;
+    ShadowMapTechnique* m_pShadowMapEffect;
     Camera* m_pGameCamera;
-    DirectionalLight m_directionalLight;
-    SkinnedMesh m_mesh;
-    SkinnedMesh m_quad;
-    Vector3f m_position;            
+    float m_scale;
+    SpotLight m_spotLight;
+    BasicMesh m_mesh;
+    BasicMesh m_quad;	
     PersProjInfo m_persProjInfo;
-    IntermediateBuffer m_intermediateBuffer;
-    Pipeline m_pipeline;
-    vector<Matrix4f> m_prevTransforms;
+    Texture* m_pGroundTex;
+    ShadowMapFBO m_shadowMapFBO;
 };
 
 
@@ -240,21 +245,17 @@ int main(int argc, char** argv)
     Magick::InitializeMagick(*argv);
     GLUTBackendInit(argc, argv, true, false);
 
-    if (!GLUTBackendCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, false, "Tutorial 41")) {
+    if (!GLUTBackendCreateWindow(WINDOW_WIDTH, WINDOW_HEIGHT, false, "Amir (42)")) {
         return 1;
     }
-    
-    SRANDOM;
-    
-    Main* pApp = new Main();
+      
+    Main App;
 
-    if (!pApp->Init()) {
+    if (!App.Init()) {
         return 1;
     }
-    
-    pApp->Run();
+        
+    App.Run();
 
-    delete pApp;
- 
     return 0;
 }
